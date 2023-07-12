043bbfd4fedd1ce6f17e6311d43cd006aaa57a4a
DaniilPak <1asumeasume1@gmail.com>
2023-07-12 17:04:18 +0600

    #bug

--- src/screens/MyLoyaltyCards.tsx
+++ src/screens/MyLoyaltyCards.tsx
@@ -24,22 +24,55 @@ function MyLoyaltyCards({ route }: MyLoyaltyCardsScreenProps) {
 
     const [isLoading, setIsLoading] = useState(true);
 
-    useEffect(() => {
+    const [isRefreshing, setIsRefreshing] = useState(false);
+
+    const onRefresh = () => {
+        try {
+            setIsRefreshing(true);
+            // Indicate that loading started
+            setIsLoading(true);
+            initFunc();
+        } catch {
+            console.log("Failed to refresh");
+        } finally {
+            setIsRefreshing(false);
+        }
+    }
+
+    const initFunc = async () => {
         getArrayFromLocalStorage(Con.API_AUTH_DATA_KEY)
             .then(asyncdata => {
                 console.log("async data my loyalty cards", asyncdata.userData.loyaltyCards);
                 setUserData(asyncdata.userData);
                 const apiLoyaltyCards = asyncdata.userData.loyaltyCards;
 
+                let tempLoyaltyCards: any[] = [];
+
                 apiLoyaltyCards.forEach((loyaltyCardId: string) => {
                     getLoyaltyCardDetails(loyaltyCardId)
                         .then(loyaltyCardDetails => {
                             console.log("++++++++", loyaltyCardDetails);
-                            setLoyaltyCards(prev => [...prev, loyaltyCardDetails]);
+                            tempLoyaltyCards.push(loyaltyCardDetails);
                         })
                         .finally(() => {
                             // Loading done
                             setIsLoading(false);
+
+                            // Sorting the array alphabetically
+                            tempLoyaltyCards.sort((a, b) => {
+                                const nameA = a.businessName.toUpperCase();
+                                const nameB = b.businessName.toUpperCase();
+
+                                if (nameA < nameB) {
+                                    return -1;
+                                }
+                                if (nameA > nameB) {
+                                    return 1;
+                                }
+                                return 0;
+                            });
+
+                            setLoyaltyCards(tempLoyaltyCards);
                         })
                         .catch(err => {
                             console.log("Can't get loyalty card details ", err);
@@ -51,6 +84,10 @@ function MyLoyaltyCards({ route }: MyLoyaltyCardsScreenProps) {
             });
 
         console.log("Loyalty cards: ", loyaltyCards);
+    }
+
+    useEffect(() => {
+        initFunc();
     }, []);
 
     const renderItem = ({ item }: any) => (
@@ -63,7 +100,9 @@ function MyLoyaltyCards({ route }: MyLoyaltyCardsScreenProps) {
                 <FlatList
                     data={loyaltyCards}
                     renderItem={renderItem}
-                    keyExtractor={(item) => item}
+                    keyExtractor={(item) => item._id}
+                    refreshing={isRefreshing}
+                    onRefresh={onRefresh}
                 />
             }
             {isLoading &&
